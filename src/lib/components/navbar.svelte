dc836c5667c8c1350d9a2e1ff9ffaeadd6ec136c
macbook pro m2 <pedroperez.ing@gmail.com>
2025-11-25 11:40:24 -0600

    carpetas creadas

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,19 @@
+<script>
+	
+</script>
+
+<nav class="principal">
+
+	
+</nav>
+
+<style>
+	.principal{
+		display: flex;
+		flex-direction: row;
+		background: blue;
+		height: 50px;
+		width: 100%;
+	}
+	
+</style>
